bd3ab333f1bae4a8d5965e5e52c80b45852f0461
Zackary Di Dio <zackary.d@yahoo.com>
2026-02-26 14:20:11 -0500

    added Employee base class with describe(), implemented Manager subclass with overridden describe(), created Company class and instantiate sample objects

---
+++ cc_6.js
@@ -0,0 +1,50 @@
+class Employee {
+    constructor(name, department) {
+        this.name = name;
+        this.department = department;
+    }
+
+    describe() {
+        return `Employee: ${this.name} works in the ${this.department} department.`;
+    }
+}
+
+class Manager extends Employee {
+    constructor(name, department, teamSize) {
+        super(name, department);
+        this.teamSize = teamSize;
+    }
+
+    describe() {
+        return `Manager: ${this.name} leads the ${this.department} department with a team of ${this.teamSize} employees.`;
+    }
+}
+
+class Company {
+    constructor() {
+        this.employees = [];
+    }
+
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+
+    listEmployees() {
+        console.log("Company Employee List:");
+        this.employees.forEach(emp => {
+            console.log(emp.describe());
+        });
+    }
+}
+
+const employee1 = new Employee("Alice Johnson", "HR");
+const employee2 = new Employee("Brian Smith", "IT");
+const manager1 = new Manager("Carol Williams", "Engineering", 5);
+
+const company = new Company();
+
+company.addEmployee(employee1);
+company.addEmployee(employee2);
+company.addEmployee(manager1);
+
+company.listEmployees();
